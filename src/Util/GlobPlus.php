ab054bc60b4adb73e5430859ddbf04fba9860d46
Tim Otten <totten@civicrm.org>
2019-08-11 17:32:55 -0700

    Add GlobPlus helper

---
+++ src/Util/GlobPlus.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Civi\AssetPlugin\Util;
+
+class GlobPlus {
+
+  // phpcs:disable
+  // civicrm/coder cannot handle empty generator syntax.
+  /**
+   * An extended variant of glob() which supports subdirectory matching
+   * with '**' as well as matching multiple patterns.
+   *
+   * @param string $baseDir
+   *   The folder to search
+   * @param string[] $pats
+   *   Each is a glob, where '*' matches a file, and '**' matching across subdirectories. Ex:
+   *   - '*.js' (match .js files in the base directory)
+   *   - '**.js' (match .js files in any descendent of the base directory)
+   *   - 'css/*.css' (match .css files directly in the 'css' subdir)
+   *   - 'css/**.css' (match .css files in any descendent of the 'css' subdir)   * @return \Generator
+   * @return \Generator
+   *   A list of matching file names, expressed relative to $baseDir.
+   *   Order is not guaranteed.
+   */
+  public static function find($baseDir, $pats) {
+    if (empty($pats)) {
+      // Effectively, return an empty generator.
+      return;
+    }
+
+    // Array(string $escapedGlob => string $unescapedRegex).
+    $globRegexMap = ['\*\*' => ".+", '\*' => '[^/]+'];
+
+    // Deterministic ordering; multiple similar calls should yield same regex.
+    sort($pats);
+
+    // This maybe a premature optimization... split regex's into 2 groups, based on which anchors are needed.
+    $regexes = [];
+    foreach ($pats as $pat) {
+      $regexes[] = strtr(preg_quote($pat, ';'), $globRegexMap);
+    }
+
+    $regex = ';^(' . implode('|', $regexes) . ')$;';
+    $baseDir = rtrim($baseDir, DIRECTORY_SEPARATOR);
+    $baseLen = strlen($baseDir) + 1;
+    $files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($baseDir, \RecursiveDirectoryIterator::SKIP_DOTS));
+
+    foreach ($files as $fileinfo) {
+      /** @var \SplFileInfo $fileinfo */
+      $relPath = substr((string) $fileinfo, $baseLen);
+      if (preg_match($regex, $relPath)) {
+        yield $relPath;
+      }
+    }
+  }
+  // phpcs:enable
+
+}
